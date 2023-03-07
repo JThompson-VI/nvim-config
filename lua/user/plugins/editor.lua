9e69d48641fed35b9e10b757c15580ac2ab26775
Jacob Thompson <a01657923@usu.edu>
2023-03-06 23:53:52 -0700

    add gitsigns keymaps

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -18,16 +18,18 @@ return {
         vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
       end
 
+      map('n', '<leader>gj', gs.next_hunk, "Next Hunk")
+      map('n', '<leader>gk', gs.prev_hunk, "Prev Hunk")
+      map('n', '<leader>gs', gs.stage_hunk, "Stage Hunk")
+      map('n', '<leader>gr', gs.reset_hunk, "Reset Hunk")
+      map('n', '<leader>gS', gs.stage_buffer, "Stage buffer")
+      map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>gl", function() gs.blame_line() end, "Blame Line")
+      -- map("n", "<leader>gl", function() gs.blame_line({ full = true }) end, "Blame Line")
+
       -- stylua: ignore start
-      --map("n", "]h", gs.next_hunk, "Next Hunk")
-      --map("n", "[h", gs.prev_hunk, "Prev Hunk")
-      --map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      --map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      --map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
       --map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-      --map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
       --map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-      --map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
       --map("n", "<leader>ghd", gs.diffthis, "Diff This")
       --map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
       --map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
