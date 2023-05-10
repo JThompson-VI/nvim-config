803e0aad83cb394218499c1b2b46aa3feadbd0c3
Jacob Thompson <a01657923@usu.edu>
2023-05-10 15:06:55 -0600

    add null-ls and formatting with prettier

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -9,3 +9,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- Uncomment to enable format on save with null-ls
+--[[ local formatting_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = formatting_augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+}) ]]
